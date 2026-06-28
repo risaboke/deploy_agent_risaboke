47790c328b4355d14b0586f5f8cfa04e66a5a0d6
Rebecca <r.isaboke@alustudent.com>
2026-06-28 10:37:09 -0700

    Add quest_06_fortune_teller.py quest

---
+++ quest_06_fortune_teller.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Quest 6: The Fortune Teller"""
+
+name = input("What is your name, chosen one? ")
+quest = input("And what is your quest? ")
+
+print(f"Greetings, {name}! Your quest to {quest} is truly heroic!")
